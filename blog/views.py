22acfea015c641beadaf5fdc8f85fe34116ff184
sitatake <tkstdic@gmail.com>
2020-03-01 17:06:49 +0900

    ok

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth.decorators import login_required
 from .models import Post,Comment
 from .forms import PostForm,CommentForm
 
+def start(request):
+    return render(request, 'blog/start.html')
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
@@ -14,7 +17,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-@login_required
+# @login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -28,7 +31,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
-@login_required
+# @login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -43,12 +46,12 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
-@login_required
+# @login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
-@login_required
+# @login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
